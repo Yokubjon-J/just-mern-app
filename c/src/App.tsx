648c03e85f05f6feda3517e845ec5f3de7331a82
1h_mmm <yoltemur@gmail.com>
2022-01-16 22:48:55 +0500

    added swipeable menu

--- c/src/App.tsx
+++ c/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
+import AdminPage from './components/AdminPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div className="App"><Outlet/></div>}> {/*w/o <Outlet/>, nesting wouldn't be possible*/}
-          <Route path="admin" element={<p>Admin</p>} />
+          <Route path="admin" element={<AdminPage/>} />
           <Route path="home" element={<p>Home</p>} />
         </Route>
       </Routes>
